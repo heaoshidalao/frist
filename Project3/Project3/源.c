08447dc79bc295c0c6d568012a35f7846b89a3d5
heaoshidalao <1546774082@qq.com>
2019-04-21 18:54:46 +0800

    提交代码

---
+++ Project3/Project3/源.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+#include <stdlib.h>
+int main()
+{
+	int year = 1000;//count = 0;
+	for(year=1000; year<=2000; year++)
+	{
+		if ((year%400==0) || (year%4==0&&year%100!=0))
+		{
+		
+			printf("%d ", year);
+		}
+	}
+	printf("\n");
+	system("pause");
+	return 0;
+}
+
