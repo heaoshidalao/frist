f5247b2698083b3b5f2caa6382b2b45eb95c156d
heaoshidalao <1546774082@qq.com>
2019-06-17 22:05:45 +0800

    提交代码

---
+++ 616/616/616.c
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//void function(int*left, int *right)
+//{
+//	while (left <right)
+//	{
+//
+//		while ((left <right)&&(*left) % 2 == 1)
+//		{
+//			left++;
+//		}
+//		while ((left <right)&&*right % 2 == 1)
+//			{
+//				right--;
+//			}
+//
+//		if(left<right)
+//		{
+//			int a = *left;
+//
+//			*left = *right;
+//			*right = a;
+//		}
+//		/*left++;
+//		right++;*/
+//		
+//	}
+//	return 0;
+//}
+//#include<stdlib.h>
+//int main(){
+//
+//	int arr[] = { 1, 2, 3, 12, 67, 32, 97, 100 };
+//	
+//
+//}
+//2.
+//void find(int arr[3][3], int*px, int *py, int key)
+//{
+//	int x = 0;
+//	int y = *py - 1;
+//	while (x < *px&&y >= 0)
+//	{
+//		if (arr[x][y]>key)
+//		{
+//			y--;
+//		}
+//		 else if (arr[x][y]<key)
+//		{
+//			x++;
+//		}
+//		 else if (arr[x][y] = key)
+//		 {
+//			 *px = x;
+//			 *py = y;
+//			 return ;
+//		 }
+//	}
+//}
+//int main()
+//{
+//	int key = 1;
+//	int x = 0;
+//	int y = 0;
+//	int arr[3][3] = {0};
+//	find(arr, &x, &y, key);
+//	
+//}
+void leftmove(char*str, int k){}
