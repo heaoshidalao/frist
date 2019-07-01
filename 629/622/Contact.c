fa203f03a994cb5a5732faeb11ec63795b42b8cb
heaoshidalao <1546774082@qq.com>
2019-07-01 13:22:28 +0800

    提交代码

---
+++ 629/622/Contact.c
@@ -0,0 +1,153 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"Contact.h"
+
+//void InitContact(Contact *pCon)
+//{
+//	int i = 0;
+//	assert(pCon != NULL);
+//	pCon->usedSize = 0;
+//	for (i = 0; i < 1000; i++)
+//	{
+//
+//	}
+//	memset(pCon->per, 0, sizeof(pCon->per));
+//
+//}
+void InitContact(Contact *pCon)
+{
+	assert(pCon != NULL);
+
+	pCon->capticty = DEFAULT_SIZE;
+	pCon->usedSize = 0;
+	pCon->per = (PersonInfo *)malloc(sizeof(PersonInfo)* pCon->capticty);
+	assert(pCon->per != NULL);
+	memset(pCon->per, 0, sizeof(PersonInfo)* pCon->capticty);
+
+}
+//0表示扩容成功，1表示矿荣失败
+static int CheckFull(Contact *pCon)
+{
+	if (pCon->usedSize == pCon->capticty)
+	{
+		
+		PersonInfo *ptr =(PersonInfo *)realloc(pCon->per, sizeof(PersonInfo)* 2 * pCon->capticty);
+		if (ptr != NULL)
+		{
+			pCon->per = ptr;
+			pCon->capticty *= 2;
+			printf("增容成功");
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+void AddContact(Contact *pCon)
+
+{
+	assert(pCon != NULL);
+	int ret = CheckFull(pCon);
+	if (ret == 0)
+	{
+		printf("扩容失败\n");
+		return;
+	}
+	printf("请输入姓名：\n");
+	scanf("%s", pCon->per[pCon->usedSize].name);
+	printf("请输入性别：\n");
+	scanf("%s", pCon->per[pCon->usedSize].sex);
+	printf("请输入年龄：\n");
+	scanf("%d", &(pCon->per[pCon->usedSize].age));
+	printf("请输入电话：\n");
+	scanf("%s", pCon->per[pCon->usedSize].tele);
+	printf("请输入住址：\n");
+	scanf("%s", pCon->per[pCon->usedSize].addr);
+
+	pCon->usedSize++;
+}
+//根据姓名查找
+int SearchContact(Contact *pCon)
+{
+	char Nam[MAX_NAME];
+	int i = 0;
+	assert(pCon != NULL);
+	if (pCon->usedSize == 0)
+	{
+		return -1;
+	}
+	printf("请输入姓名：\n");
+	scanf("%s", Nam);
+	for (i = 0; i < pCon->usedSize; i++)
+	{
+		if (strcmp(Nam, pCon->per[i].name) == 0)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+//删除
+void DelContact(Contact *pCon)
+{
+	int i = 0;
+	int index = SearchContact(pCon);
+
+	if (-1 == index)
+	{
+		printf("没有此人！\n");
+		return;
+	}
+
+	for (i = index; i < pCon->usedSize - 1; i++)
+	{
+		pCon->per[i] = pCon->per[i + 1];
+	}
+	pCon->usedSize--;
+	printf("删除成功！\n");
+
+}
+
+void ShowContact(Contact *pCon)
+{
+	int i = 0;
+	assert(pCon != NULL);
+	printf("%-20s %-10s  %-10s %-11s %-15s\n", "姓名", "性别", "年龄", "电话", "地址");
+
+	for (i = 0; i < pCon->usedSize ; i++)
+	{
+		printf("%-20s%-10s%-10d%-11s%-15s\n", pCon->per[i].name, pCon->per[i].sex, pCon->per[i].age, pCon->per[i].tele, pCon->per[i].addr);
+
+	} 
+
+}
+
+void bubble_sort(Contact *pCon)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < pCon->usedSize - 1; i++)
+	{
+		int j = 0;
+		for (j = 0; j < pCon->usedSize - 1 - i; j++)
+		{
+			if (strcmp(pCon->per[j].name, pCon->per[j + 1].name)>0)
+			{
+				PersonInfo temp;
+				temp = pCon->per[j];
+				pCon->per[j] = pCon->per[j + 1];
+				pCon->per[j + 1] = temp;
+			}
+		}
+	}
+}
+void DestroyContact(Contact *pCon)
+{
+	assert(pCon != NULL);
+	free(pCon->per);
+	pCon->per = NULL;
+	pCon->capticty;
+	pCon->usedSize = 0;
+}
---
+++ 629/622/Contact.h
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#ifndef _CONTACT_H_
+#define _CONTACT_H_
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
+#define MAX_NAME 20
+#define MAX_TEL 11
+#define MAX_ADDR 15
+#define MAX_SEX 3
+#define MAX_NUMPERSON 1000
+
+#define DEFAULT_SIZE 2
+enum Option
+{
+	Exit,
+	add,
+	search,
+	del,
+	show,
+	clear,
+	sort,
+	destroy,
+};
+
+typedef struct PersonInfo
+{
+	char name[MAX_NAME];
+	char sex[MAX_SEX];
+	short age;
+	char tele[MAX_TEL];
+	char addr[MAX_ADDR];
+}PersonInfo;
+
+//typedef struct Contact
+//{
+//	PersonInfo per[MAX_NUMPERSON];
+//	int usedSize;//被使用的个数
+//}Contact;
+
+typedef struct Contact
+{/*
+	PersonInfo per[MAX_NUMPERSON];*/
+	PersonInfo *per;
+
+	int usedSize;//被使用的个数
+	int capticty;//容量
+}Contact;
+void InitContact(Contact *pCon);
+void AddContact(Contact *pCon);
+int SearchContact(Contact *pCon);
+void DelContact(Contact *pCon);
+void ShowContact(Contact *pCon);
+void bubble_sort(Contact *pCon);
+ void DestroyContact(Contact *pCon);
+#endif
