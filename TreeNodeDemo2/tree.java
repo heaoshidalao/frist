99951a522d33be8b9c4faa40b85a23b6b65c2c64
heaoshidalao <1546774082@qq.com>
2019-11-22 22:56:06 +0800

    代码

--- TreeNodeDemo2/tree.java
+++ TreeNodeDemo2/tree.java
@@ -74,3 +74,53 @@ public class tree {
 //        return (left == null)?pRootOfTree:left;
 //    }
 //}
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int index = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // 每次构建之前要置为 0, 因为在线 OJ 有多组测试
+        index = 0;
+        // 在这个递归过程中, 需要处理到某个具体的子树.
+        // [0, inorder.length) 当前处理的子树对应的中序遍历结果是什么
+        return buildTreeHelper(preorder, inorder, 0, inorder.length);
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int[] inorder,
+                                     int inorderLeft, int inorderRight) {
+        // 先判定非法情况
+        if (inorderLeft >= inorderRight) {
+            // 这个子树的中序遍历是没有的. 这就是个空子树
+            return null;
+        }
+        if (index >= preorder.length) {
+            return null;
+        }
+        // 取出当前值构造当前子树的根节点
+        TreeNode root = new TreeNode(preorder[index]);
+        index++;  // 取完这个节点, 就可以取下一个节点了
+        // 需要找到这个节点在中序遍历中的位置.
+        // pos 只要说 先序遍历 和 中序遍历 序列都是对的.
+        // pos 是一定能找到的.
+        int pos = find(inorder, inorderLeft, inorderRight, root.val);
+        root.left = buildTreeHelper(preorder, inorder, inorderLeft, pos);
+        root.right = buildTreeHelper(preorder, inorder, pos + 1 , inorderRight);
+        return root;
+    }
+
+    private int find(int[] inorder, int inorderLeft, int inorderRight, int val) {
+        for (int i = inorderLeft; i < inorderRight; i++) {
+            if (inorder[i] == val) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
