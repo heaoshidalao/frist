007894400891de3e2b4a066d7fa7975a2b895f0d
heaoshidalao <1546774082@qq.com>
2020-09-01 22:45:57 +0800

    刷题

---
+++ jianzhi0901/jianzhi15.java
@@ -0,0 +1,31 @@
+package jianzhi0901;
+
+public class jianzhi15 {
+//    /*输入一个链表，反转链表后，输出新链表的表头。
+//public class ListNode {
+//    int val;
+//    ListNode next = null;
+//
+//    ListNode(int val) {
+//        this.val = val;
+//    }
+//}*/
+////import java.util.ArrayList;
+//    public class Solution {
+//        public ListNode ReverseList(ListNode head) {
+//            if(head == null){
+//                return null;
+//            }
+//            ListNode pre = null;
+//            ListNode cur = head;
+//            ListNode next = null;
+//            while(cur!=null){
+//                next = cur.next;
+//                cur.next = pre;
+//                pre = cur;
+//                cur = next;
+//            }
+//            return pre;
+//        }
+   // }
+}
