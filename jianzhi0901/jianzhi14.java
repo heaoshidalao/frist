5656b9fe51a0c43d4e3a8a335d77e9b8098ff7d7
heaoshidalao <1546774082@qq.com>
2020-09-01 21:11:43 +0800

    1

---
+++ jianzhi0901/jianzhi14.java
@@ -0,0 +1,40 @@
+package jianzhi0901;
+
+public class jianzhi14 {
+//    /*
+//public class ListNode {
+//    int val;
+//    ListNode next = null;
+//
+//    ListNode(int val) {
+//        this.val = val;
+//    }
+//}*/
+//    public class Solution {
+//        public ListNode FindKthToTail(ListNode head,int k) {
+//            if(head == null){
+//                return null;
+//            }
+//            ListNode node = head;
+//            int count = 0;
+//            while(node!=null){
+//                node = node.next;
+//                count++;
+//            }
+//
+//            if(k>count){
+//                return null;
+//            }
+//            ListNode fast = head;
+//            ListNode slow = head;
+//            for(int i = 0;i<k;i++){
+//                fast = fast.next;
+//            }
+//            while(fast!=null){
+//                fast = fast.next;
+//                slow = slow.next;
+//            }
+//            return slow;
+//        }
+//    }
+}
