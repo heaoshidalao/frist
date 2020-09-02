9b4f36d30fd92e76722f1da1f822764fdf202f5b
heaoshidalao <1546774082@qq.com>
2020-09-02 22:14:28 +0800

    做题

---
+++ jianzhi0902/jianzhi16.java
@@ -0,0 +1,74 @@
+package jianzhi0902;
+
+public class jianzhi16 {
+//    ListNode pre = null;
+//    ListNode newNode = null;
+//        if(list1 == null){
+//        return list2;
+//    }
+//        if(list2 == null){
+//        return list1;
+//    }
+//       if(list1.val <= list2.val){
+//        newNode = list1;
+//        pre = newNode;
+//        newNode = newNode.next;
+//        list1 = list1.next;
+//    }
+//            else{
+//        newNode = list2;
+//        pre = newNode;
+//        newNode = newNode.next;
+//        list2 = list2.next;
+//    }
+//        while(list1 != null&&list2 !=  null){
+//        if(list1.val <= list2.val){
+//            newNode = list1;
+//            newNode = newNode.next;
+//            list1 = list1.next;
+//        }
+//        else{
+//            newNode = list2;
+//            newNode = newNode.next;
+//            list2 = list2.next;
+//        }
+//    }
+//        while(list1!= null){
+//        newNode = list1;
+//        newNode = newNode.next;
+//        list1 = list1.next;
+//    }
+//        while(list2!= null){
+//        newNode = list2;
+//        newNode = newNode.next;
+//        list2 = list2.next;
+//    }
+//        return pre;
+//public ListNode Merge(ListNode list1,ListNode list2) {
+//    ListNode head = new ListNode(-1);
+//    ListNode  cur = head;
+//    while(list1!= null&&list2!= null){
+//        if(list1.val<=list2.val){
+//            cur.next = list1;
+//            list1 = list1.next;
+//            cur = cur.next;
+//        }
+//        else{
+//            cur.next = list2;
+//            list2 = list2.next;
+//            cur = cur.next;
+//
+//        }
+//    }
+//    while(list1!=null){
+//        cur.next = list1;
+//        list1 = list1.next;
+//        cur = cur.next;
+//    }
+//    while(list2!= null){
+//        cur.next = list2;
+//        list2 = list2.next;
+//        cur = cur.next;
+//    }
+//    return head.next;
+}
