70d1d6cf02842c968f3fd4279a1adf05856b95a6
heaoshidalao <1546774082@qq.com>
2020-09-06 22:03:41 +0800

    顺时针打印矩阵

---
+++ jianzhi0906/jianzhi19.java
@@ -0,0 +1,40 @@
+package jianzhi0906;
+
+public class jianzhi19 {
+//    import java.util.ArrayList;
+//    public class Solution {
+//        public ArrayList<Integer> printMatrix(int [][] matrix) {
+//            ArrayList<Integer> result = new ArrayList<>();
+//            if(matrix == null){
+//                return result;
+//            }
+//            int low = 0;
+//            int high = matrix.length-1;
+//            int left = 0 ;
+//            int right = matrix[0].length -1;
+//            while(low <= high&&left <= right){
+//                for(int i = left;i <= right;i++ ){
+//                    result.add(matrix[low][i]);
+//                }
+//                for(int i = low+1  ;i<= high ;i++){
+//                    result.add(matrix[i][right]);
+//                }
+//                if(low < high){
+//                    for(int i = right - 1;i >=left;i-- ){
+//                        result.add(matrix[high][i]);
+//                    }
+//                }
+//                if(left<right){
+//                    for(int i = high-1;i>=low+1;i--){
+//                        result.add(matrix[i][left]);
+//                    }
+//                }
+//                right--;
+//                left++;
+//                low++;
+//                high--;
+//            }
+//            return result;
+//        }
+ //   }
+}
