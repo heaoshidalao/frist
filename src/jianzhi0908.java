6329b42d69ed94aa65d3a8e34c293b990844a54e
heaoshidalao <1546774082@qq.com>
2020-09-08 20:22:57 +0800

    111

---
+++ src/jianzhi0908.java
@@ -0,0 +1,29 @@
+public class jianzhi0908 {
+    /*
+//public class ListNode {
+//    int val;
+//    ListNode next = null;
+//
+//    ListNode(int val) {
+//        this.val = val;
+//    }
+//}*/
+//    public class Solution {
+//        public ListNode ReverseList(ListNode head) {
+//            if(head == null){
+//                return null;
+//            }
+//            ListNode pre = null;
+//            ListNode node = head;
+//            ListNode next = null;
+//            while(node != null){
+//                next = node.next;
+//                node.next = pre;
+//                pre = node;
+//                node = next;
+//
+//            }
+//            return pre;
+//        }
+  //  }
+}
