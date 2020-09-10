fea624e0f6bde533fbbc37ba1add845ed967c161
heaoshidalao <1546774082@qq.com>
2020-09-10 22:24:12 +0800

    11

---
+++ src/xiecheng.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+public class TreeNode {
+     int val = 0;
+     TreeNode next= null;
+     public TreeNode(int val) {
+     this.val = val;
+
+     }
+
+public class xiecheng {
+    public  void Main(String [] args){
+        Scanner  sc = new Scanner(System.in);
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        while (!has.nextInt())
+
+    }
+
+}
